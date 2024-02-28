6351a8bbda44b44bd9d68b8294cafd4b25a0edff
Espen <espenpede@rocketmail.com>
2024-02-28 08:59:23 +0100

    articles and main site

--- src/components/home/Articles/_components/ArticleCards/index.tsx
+++ src/components/home/Articles/_components/ArticleCards/index.tsx
@@ -1,11 +1,15 @@
 import { Article } from "@/types";
 import ArticleCard from "@/components/shared/ArticleCard";
-import { articles } from "@/lib/articles";
+import { sanityFetch } from "@/functions/getData";
+import { articleQuery } from "@/lib/queries";
 
 const ArticleCards = async () => {
+  const query = articleQuery;
+  const data = await sanityFetch(query);
+
   return (
     <div>
-      {articles.map((article: Article, idx: number) => {
+      {data.map((article: Article, idx: number) => {
         return (
           <ArticleCard
             key={idx}
--- src/components/home/Articles/index.tsx
+++ src/components/home/Articles/index.tsx
@@ -3,10 +3,6 @@ import ArticleCards from "./_components/ArticleCards";
 const Articles = () => {
   return (
     <section className="section max-width">
-      <div>
-        <h3>På jakt etter ny elsykkel?</h3>
-        <h4>Kom innom oss for en hyggelig elsykkelprat</h4>
-      </div>
       <ArticleCards />
     </section>
   );
